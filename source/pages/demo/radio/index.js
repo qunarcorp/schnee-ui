72674d17105d4fa6f8605ad08f085e505da11abb
codearvin <codearvin@gmail.com>
2018-12-05 20:12:11 +0800

    fix: radio 快应用兼容 && 添加 README.md

--- source/pages/demo/radio/index.js
+++ source/pages/demo/radio/index.js
@@ -1,5 +1,5 @@
 import React from '@react';
-// import './index.scss';
+import './index.scss';
 import Radio from '@components/Radio/index';
 import RadioGroup from '@components/RadioGroup/index';
 class P extends React.Component {
