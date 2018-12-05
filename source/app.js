5852fff666e763dd91db31d2094f3b889e6b7fe9
tianqi.tian <tianqi.tian@quanr.com>
2018-12-05 16:12:51 +0800

    feat: update for quick app

--- source/app.js
+++ source/app.js
@@ -1,21 +1,16 @@
 import React from '@react';
+
 import './pages/index/index';
+
 import './pages/demo/button/index';
 import './pages/demo/checkbox/index';
-import './pages/demo/input/index';
-import './pages/demo/slider/index';
-import './pages/demo/picker/index';
-import './pages/demo/radio/index';
-import './pages/demo/textarea/index';
 import './pages/demo/label/index';
-import './pages/demo/view/index';
-import './pages/demo/scrollView/index';
-import './pages/demo/movableView/index';
-import './pages/demo/image/index';
-import './pages/demo/video/index';
+import './pages/demo/picker/index';
 import './pages/demo/progress/index';
-import './pages/demo/switch/index';
+import './pages/demo/radio/index';
+import './pages/demo/slider/index';
 import './pages/demo/swiper/index';
+import './pages/demo/switch/index';
 
 import './app.scss';
 
@@ -26,11 +21,16 @@ class Global extends React.Component {
             // navigationBarBackgroundColor: '#0088a4',
             navigationBarTitleText: 'Schnee UI',
             navigationBarTextStyle: '#fff'
-        },
+        }
     };
+    onLaunch() {
+        // eslint-disable-next-line
+        console.log('App launched');
+    }
 }
 //这样写相当于为每一个页面组件的外面都加上一个<Provider />，如果你想在页面上用到store里的数据，
 //需要用react-redux的connect方法包一下，详见pages/demo/syntax/redux
 //React.applyAppStore(store);
 // eslint-disable-next-line
 export default App(new Global());
+
