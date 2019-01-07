81c15b8ff60ddf048342166d507adfb5135acf24
tianqi.tian <tianqi.tian@quanr.com>
2019-01-07 20:08:48 +0800

    Use relative path

--- components/XPicker/index.js
+++ components/XPicker/index.js
@@ -1,8 +1,8 @@
 import React from '@react';
 import './index.scss';
-import XOverlay from '@components/XOverlay/index';
-import XPickerItem from '@components/XPickerItem/index';
-import XDatePickerItem from '@components/XDatePickerItem/index';
+import XOverlay from '../XOverlay/index';
+import XPickerItem from '../XPickerItem/index';
+import XDatePickerItem from '../XDatePickerItem/index';
 import { nextDate, timeStrToDate, getDate, nextMinute, getTime } from '../../common/utils/time';
 import cnCity from '../../common/utils/cnCity';
 /* eslint-disable */
