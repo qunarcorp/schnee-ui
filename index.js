525d0a815594fdc530995289959dadf6dfe0aea3
tianqi.tian <tianqi.tian@quanr.com>
2019-01-07 16:23:38 +0800

    Publish

---
+++ index.js
@@ -0,0 +1,33 @@
+import XButton from './components/XButton';
+import XCheckbox from './components/XCheckbox';
+import XCheckboxGroup from './components/XCheckboxGroup';
+import XIcon from './components/XIcon';
+import XLabel from './components/XLabel';
+import XNavigator from './components/XNavigator';
+import XPicker from './components/XPicker';
+import XProgress from './components/XProgress';
+import XRadio from './components/XRadio';
+import XRadioGroup from './components/XRadioGroup';
+import XRichText from './components/XRichText';
+import XScrollView from './components/XScrollView';
+import XSlider from './components/XSlider';
+import XSwiper from './components/XSwiper';
+import XSwitch from './components/XSwitch';
+
+export default {
+    XButton,
+    XCheckbox,
+    XCheckboxGroup,
+    XIcon,
+    XLabel,
+    XNavigator,
+    XPicker,
+    XProgress,
+    XRadio,
+    XRadioGroup,
+    XRichText,
+    XScrollView,
+    XSlider,
+    XSwiper,
+    XSwitch
+};
