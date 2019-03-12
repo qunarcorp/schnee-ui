e3c9f91d9fb5dc71de30a3bd6682f1828eaee3a9
jingjing.geng <jingjing.geng@qunar.com>
2019-03-12 16:00:12 +0800

    fix: 修改规范

---
+++ bin/dev.sh
@@ -0,0 +1,2 @@
+rm -rf _sourceDev/*
+cp -rf source/* _sourceDev
---
+++ source/componentInfo.js
@@ -0,0 +1,21 @@
+
+module.exports = {
+    componentsArr: [
+        'XButton',
+        'XCheckbox',
+        'XCheckboxGroup',
+        'XIcon',
+        'XLabel',
+        'XNavigator',
+        'XPicker',
+        'XProgress',
+        'XRadio',
+        'XRadioGroup',
+        'XRichText',
+        'XScrollView',
+        'XSlider',
+        'XSwiper',
+        'XSwitch',
+        'XWebview'
+    ]
+};
